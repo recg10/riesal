80feebf8ff2f8b5c196e11921189f77425711696
recg10 <recg10@hotmail.com>
2018-08-08 23:39:16 -0400

    prueba

--- ict/ictLogic/src/main/java/cl/devap/ictLogic/service/MailService.java
+++
@@ -1,9 +0,0 @@
-package cl.devap.ictLogic.service;
-
-import cl.devap.ict.exception.IctException;
-
-public interface MailService  {
-	
-	public void enviarMail(String destinatario, String asunto, String cuerpo) throws IctException;
-	
-}
--- ict/ictWeb/src/main/java/cl/devap/facol/bean/BeanGenerico.java
+++
@@ -1,7 +0,0 @@
-package cl.devap.facol.bean;
-
-import java.io.Serializable;
-
-public class BeanGenerico implements Serializable{
-
-}
